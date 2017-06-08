b6f5f0c0a377a75353800590e2a2fc912bcec969
Sagar Sharma <sagarsharma708@gmail.com>
2017-06-09 04:26:12 +0530

    Second commit

--- app/src/main/java/com/project/verbosetech/findout/MyApp.java
+++ app/src/main/java/com/project/verbosetech/findout/MyApp.java
@@ -1,6 +1,8 @@
 package com.project.verbosetech.findout;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatDelegate;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -24,5 +26,11 @@ public class MyApp extends Application {
                 .setFontAttrId(R.attr.fontPath)
                 .build()
         );
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
